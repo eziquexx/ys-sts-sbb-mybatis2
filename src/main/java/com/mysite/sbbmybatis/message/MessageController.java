e04c123d2ed12295084524a0ac426e61c0080e82
eziquexx <waftyann@gmail.com>
2024-10-14 23:27:53 +0900

    @RequestMapping 추가

--- src/main/java/com/mysite/sbbmybatis/message/MessageController.java
+++ src/main/java/com/mysite/sbbmybatis/message/MessageController.java
@@ -4,19 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mysite.sbbmybatis.dto.Message;
 
 @Controller
+@RequestMapping("/messages")
 public class MessageController {
 	@Autowired
 	private MessageService messageService;
 	
-	//detail
-	@GetMapping("/messages/{id}")
+	// detail
+	@GetMapping("/{id}")
 	@ResponseBody
 	public Message getMessage(@PathVariable("id") Integer id) {
 		return messageService.getMessage(id);
 	}
+	
+
 }
